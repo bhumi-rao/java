4f518fa309db7486eabbb7bae0e2676c6f5c08cd
Bhumika D <151423873+bhumi-rao@users.noreply.github.com>
2025-08-07 22:24:55 +0530

    Update sortarr.java

--- sortarr.java
+++ sortarr.java
@@ -1,1 +1,14 @@
+import java.util.Arrays;
+
+public class sortarr {
+    public static void main(String[] args) {
+        int[] arr = {25, 10, 5, 30, 15};
+        Arrays.sort(arr);  // built-in sort
+
+        System.out.print("Sorted array: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
 
